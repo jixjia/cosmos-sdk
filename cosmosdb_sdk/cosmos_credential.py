e8db14dd849721dbba7b1eda02c1e83283a19e66
Jixin Jia <jixin.jia@outlook.com>
2020-02-20 17:02:10 +0900

    Removed credentials

--- cosmosdb_sdk/cosmos_credential.py
+++ cosmosdb_sdk/cosmos_credential.py
@@ -2,6 +2,6 @@ import os
 
 # Cosmos DB config
 cosmosdb_config = {
-    'ENDPOINT': 'https://jixjia-cosmos.documents.azure.com:443/',
-    'PRIMARYKEY': 'BIxCe9iwZJ2x2XRY6Z7gBPL81blQCuUe1vSkXbVmXr88L00VYXId5GxO72bdCxZ7ctZ9CaYDLk9hXSiG3AYqtA=='
+    'ENDPOINT': '<YOUR_COSMOS_ENDPOINT>',
+    'PRIMARYKEY': '<YOUR_PRIMARY_KEY>'
 }
